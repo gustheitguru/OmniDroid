1a0cf98bbeb96ef2e34a4a40991fdcedbf139268
Gus <gus@theyogidude.com>
2023-11-10 15:16:22 -0800

    Adding Bootstrap

--- onmif/src/main.js
+++ onmif/src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// Import Bootstrap and BootstrapVue CSS
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Import BootstrapVue 3 plugin
+import BootstrapVue3 from 'bootstrap-vue-3';
+
+const app = createApp(App);
+
+// Use BootstrapVue 3 plugin
+app.use(BootstrapVue3);
+
+app.mount('#app');
+
+
+// OG Code?
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
